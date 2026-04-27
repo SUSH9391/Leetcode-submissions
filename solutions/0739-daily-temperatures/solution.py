c74a340060ec9dcdbdb7cb973aca279585038161
Sushmitha Baliga B <94959102+SUSH9391@users.noreply.github.com>
2026-04-27 16:19:17 +0000

    Sync LeetCode submission Runtime - 89 ms (70.99%), Memory - 28.3 MB (70.54%)

---
+++ solutions/0739-daily-temperatures/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        n = len(temperatures)
+        answer = [0] * n
+        stack = []
+        for i in range(n):
+            while stack and temperatures[i] > temperatures[stack[-1]]:
+                prv = stack.pop()
+                answer[prv] = i - prv
+            
+            stack.append(i)
+        return answer
