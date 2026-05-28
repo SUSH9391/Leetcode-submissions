6c92a429eb8cf3149f651b3be105bf112073089d
Sushmitha Baliga B <94959102+SUSH9391@users.noreply.github.com>
2026-05-28 16:12:28 +0000

    [LeetCode Sync] Runtime - 33 ms (94.24%), Memory - 53.8 MB (35.78%)

---
+++ leetcode-solutions/2732-counter/solution.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+ 
+var createCounter = function(n) {
+    let x = -1 ;
+   return function(){
+    x++
+    return n+x
+     
+    }
+};
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
