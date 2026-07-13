1372d509d493a76b098b0f6e8c8e43c6e16842ae
Sushmitha Baliga B <94959102+SUSH9391@users.noreply.github.com>
2026-07-13 16:45:26 +0000

    [LeetCode Sync] Runtime - 3 ms (63.03%), Memory - 20.4 MB (46.51%)

---
+++ leetcode-solutions/0563-binary-tree-tilt/solution.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findTilt(self, root: Optional[TreeNode]) -> int:
+        tilt = 0
+        def dfs(node):
+            nonlocal tilt
+            if node is None:
+                return 0
+            left_sum = dfs(node.left)
+            right_sum = dfs(node.right)
+            tilt += abs(left_sum - right_sum)
+            return left_sum + right_sum +node.val
+        dfs(root)
+        return tilt
