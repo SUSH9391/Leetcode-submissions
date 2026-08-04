2b8ff40c2b54d451a4982f02fc7f819e54567db9
Sushmitha Baliga B <94959102+SUSH9391@users.noreply.github.com>
2026-08-04 07:18:37 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 20.5 MB (42.50%)

--- Leetcode-submissions/0001-two-sum/solution.py
+++ Leetcode-submissions/0001-two-sum/solution.py
@@ -1,10 +1,14 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        prevMap = {} # val : index
-
+        #one way is to store the indices of the numbers in the hashmap and return them since we are fetching theindices from a hasmap and at the same iterating the complete array to store the indices to the hashmap we are running into 
+        #time complexity of O(n)
+        #space complexity O(n) since we are using extra memory of hashmap and storing the numbers: indices
+        indices = {} # initalized an array to hold {number : indices}
+        #since we need both the value and their indices we are gonna be using enumerate
         for i, n in enumerate(nums):
-            diff = target - n
-            if diff in prevMap:
-                return [prevMap[diff], i]
-            prevMap[n] = i
-        return
+            #calculate how much more is needed to build the target number
+            compliment = target - n
+            if compliment in indices:
+                return [indices[compliment], i] # we are supposed to return the array of the compliment number and the current number we are looking at which are capable to make a sum equal to target
+            indices[n] = i #example {n-4: i-2}
+        return []
