994b25a08239c0b6676b41c7631b9cf58bdff3b7
Sushmitha Baliga B <94959102+SUSH9391@users.noreply.github.com>
2026-09-03 12:14:45 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.4 MB (18.27%)

--- solutions/0070-climbing-stairs/solution.py
+++ solutions/0070-climbing-stairs/solution.py
@@ -1,14 +1,12 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        tee, too = 1,1
+        if n <= 2:
+            return n
+        prev2,prev1 = 1, 2
+        for i in range(3, n+1):
+            curr = prev1 + prev2
+            prev2 = prev1
+            prev1 = curr
+        return prev1
 
-        #im particularly dynamic programing approach and here im just using an extra space 
-        #my very variable names are tee and too so tee points to n-1th element and too points to nth element they are initally one since when we analyse the pattern always the nth and the n-1th step is always 1 simialr to n=0 and n=1 with op as 1
-        #what are the next steps that are n-2, n-3 and so on we add the 2 already computed results and move the variable using a temp variable just like fibonnaci series
-        for i in range(n-1):
-            temp = tee
-            tee += too
-            too = temp
-        return tee
-            
-            
+
