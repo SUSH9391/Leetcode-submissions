ef25201756df435292b045a3b491149a8f926ad3
Sushmitha Baliga B <94959102+SUSH9391@users.noreply.github.com>
2026-09-04 11:00:22 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.6 MB (17.84%)

--- leetcode-solutions/0300-longest-increasing-subsequence/solution.py
+++ leetcode-solutions/0300-longest-increasing-subsequence/solution.py
@@ -1,8 +1,17 @@
+import bisect
+
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        LIS = [1] *len(nums)
-        for i in range(len(nums)-1,-1,-1):
-            for j in range(i+1, len(nums)):
-                if nums[i] < nums[j]:
-                    LIS[i] = max(LIS[i], 1+LIS[j])
-        return max(LIS)
+        sub = []
+        for num in nums:
+            # Find the index of the smallest element >= num
+            idx = bisect.bisect_left(sub, num)
+            
+            # If num is larger than all elements in sub, append it
+            if idx == len(sub):
+                sub.append(num)
+            else:
+                # Otherwise, replace the existing element to keep potential tails small
+                sub[idx] = num
+                
+        return len(sub)
