3971b78fa39cffabd5a7e2640bfd290e677a5563
Sushmitha Baliga B <94959102+SUSH9391@users.noreply.github.com>
2026-09-04 06:57:21 +0000

    [LeetCode Sync] Runtime - 265 ms (56.60%), Memory - 19.4 MB (37.17%)

--- leetcode-solutions/0005-longest-palindromic-substring/solution.py
+++ leetcode-solutions/0005-longest-palindromic-substring/solution.py
@@ -2,17 +2,18 @@ class Solution:
     def longestPalindrome(self, s: str) -> str:
         if not s:
             return ""
-        start, max_len = 0,0
-        def expand_around_center(left : int, right: int) -> int:
-            while left >= 0 and right < len(s) and s[left] == s[right]:
+        start = 0 
+        max_len = 0
+        def expand(left,right):
+            while left >= 0 and right< len(s) and s[left] ==s[right]:
                 left -= 1
                 right += 1
-            return right - left -1
+            return right - left - 1
         for i in range(len(s)):
-            len1 = expand_around_center(i, i)
-            len2 = expand_around_center(i, i+1)
-            curr_len = max(len1, len2)
-            if curr_len > max_len:
-                max_len = curr_len
-                start = i - (curr_len - 1) // 2
-        return s[start:start + max_len] 
+            len_odd_string = expand(i,i)
+            len_even_string = expand(i,i+1)
+            cur_max = max(len_odd_string,len_even_string)
+            if cur_max > max_len:
+                max_len = cur_max
+                start = i-(cur_max - 1)//2
+        return s[start: start + max_len]
