d9141ff3d2fe89e2bb10fedad93c8f38cb43321c
Sushmitha Baliga B <94959102+SUSH9391@users.noreply.github.com>
2026-09-04 07:20:06 +0000

    [LeetCode Sync] Runtime - 111 ms (85.20%), Memory - 19.1 MB (98.77%)

--- Leetcode-submissions/0647-palindromic-substrings/solution.py
+++ Leetcode-submissions/0647-palindromic-substrings/solution.py
@@ -1,17 +1,16 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
+        if not s:
+            return 0
         res = 0
-        
         for i in range(len(s)):
-            res += self.countPali(s,i,i)
-            res += self.countPali(s,i,i+1)
+            res += self.isPali(s,i,i) #odd lenght
+            res += self.isPali(s,i,i+1) #even lenght
         return res
-        
-    def countPali(self,s,l,r):
+    def isPali(self,s,l,r):
         res = 0
         while l >= 0 and r < len(s) and s[l] == s[r]:
-            res += 1
+            res+=1
             l -= 1
             r += 1
         return res
-
