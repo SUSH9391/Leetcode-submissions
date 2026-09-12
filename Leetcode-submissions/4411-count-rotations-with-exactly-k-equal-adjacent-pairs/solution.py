f8d6a098ac2328f0381771b65461a82f1de06776
Sushmitha Baliga B <94959102+SUSH9391@users.noreply.github.com>
2026-09-12 04:37:30 +0000

    [LeetCode Sync] Runtime - 67 ms (48.70%), Memory - 19.3 MB (69.35%)

--- Leetcode-submissions/4411-count-rotations-with-exactly-k-equal-adjacent-pairs/solution.py
+++ Leetcode-submissions/4411-count-rotations-with-exactly-k-equal-adjacent-pairs/solution.py
@@ -1,20 +1,13 @@
 class Solution:
     def countRotations(self, s: str, k: int) -> int:
-        #whenever we rotate the strings the adjeacent pain of strings if equal will score to 0 so this is one of the base case
-        #another base case is we return the score which is equal to k k is given in the input 
-        #if k == no_of_equal:
-            #res = number of unequal pairs
-        #elif k == e-1:
-        #res= no of equal edges
-        #else : 0
         n = len(s)
-        if n ==0:
-            return 0
-        eq = sum (s[i] == s[(i+1)% n] for i in range(n))
-        if k == eq:
-            return n- eq
-        elif k ==eq -1:
-            return eq
-        else:
-            return 0
-        
+        valid_count = 0
+        for _ in range(n):
+            score = 0
+            for i in range(n-1):
+                if s[i] == s[i+1]:
+                    score += 1
+            if score == k:
+                valid_count += 1
+            s = s[1:] + s[0]
+        return valid_count
