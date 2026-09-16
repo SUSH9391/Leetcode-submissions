5fe624d8e30054b049bd951addbb88213ade13c3
Sushmitha Baliga B <94959102+SUSH9391@users.noreply.github.com>
2026-09-16 06:30:48 +0000

    [LeetCode Sync] Runtime - 3 ms (41.33%), Memory - 19.3 MB (52.60%)

--- Leetcode-submissions/0067-add-binary/solution.py
+++ Leetcode-submissions/0067-add-binary/solution.py
@@ -1,15 +1,15 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        res = []
+        res = ""
+        a,b = a[::-1] , b[::-1]
         carry = 0
-        i, j = len(a) -1,len(b) - 1
-        while i>=0 or j>=0 or carry:
-            val_a = int(a[i]) if i>= 0 else 0
-            val_b = int(b[j]) if j>= 0 else 0
-
-            total = val_a + val_b +carry
-            res.append(str(total % 2))
+        for i in range(max(len(a), len(b))):
+            digitA = ord(a[i]) - ord("0") if i <len(a) else 0
+            digitB = ord(b[i]) - ord("0") if i < len(b) else 0
+            total = digitA + digitB + carry
+            char = str(total % 2) #convert int to binary
+            res = char + res
             carry = total // 2
-            i-=1
-            j-=1
-        return "".join(reversed(res))
+        if carry:
+            res = "1" + res
+        return res
