8a57260fcb088a602cb5385d9e04b70e0563eb7d
Sushmitha Baliga B <94959102+SUSH9391@users.noreply.github.com>
2026-09-19 07:12:44 +0000

    [LeetCode Sync] Runtime - 19 ms (73.32%), Memory - 26.7 MB (34.77%)

--- leetcode-solutions/0238-product-of-array-except-self/solution.py
+++ leetcode-solutions/0238-product-of-array-except-self/solution.py
@@ -1,14 +1,11 @@
 class Solution:
-    def productExceptSelf(self, nums: List[int]) -> List[int]:
-        #since we are talking about the product we are supposed to initalize the array in such a way that its pre element places are supposed to be initalized to 1 coz 0* anything is zero
-        res = [1] * len(nums)
-        prevProd = 1
-        postProd = 1
+    def productExceptSelf(self, nums: list[int]) -> list[int]:
+        output = [1]
+        for i in range(len(nums)-1, 0, -1):
+            output.append(output[-1]*nums[i])
+        output = output[::-1]
+        left = 1
         for i in range(len(nums)):
-            res[i] *= prevProd # we need to keep updated the prefix initally the prefix is 1 coz the 1st element has no prefix element 
-            prevProd *= nums[i] #[1, 2, 3] 1*1, 1*2(for nums(1)) 2*3(for nums(2))
-        for i in range(len(nums)-1, -1, -1):
-            res[i] *= postProd
-            postProd *= nums[i]
-        return res
-       
+            output[i] = output[i] * left
+            left *= nums[i]
+        return output
